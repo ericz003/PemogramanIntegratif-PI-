e26e85c01f95cbd2ae30ef2ff5ae55ca384d098e
ericz003 <76954409+ericz003@users.noreply.github.com>
2021-03-04 18:36:13 +0700

    Commit 1

---
+++ Pengecekan MinusPositif.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char karakter;
+    char ulang;
+  
+    
+//awal:
+   cout<<"Program untuk menentukan bil positif atau negatif"<<endl;
+    cout<<"Masukkan Angka : ";
+    cin>>karakter;
+    //if (karakter >='A' && karakter <='Z'){
+	
+       // cout << "Masukkan Anda adalah huruf Tolong diganti yaaa";
+    //}
+    //else if (karakter >='a' && karakter <='z'){
+	
+       // cout << "Masukkan Anda adalah huruf Tolong diganti yaaa";
+		//}
+     if (karakter <'0'){
+	
+        cout << "Ini angka minus"<<endl;
+		}
+    else if (karakter =='0'){
+	
+        cout << "Ini adalah 0"<<endl;
+		}
+    else if (karakter >='1' && karakter <='9'){
+	
+        cout << "Masukkan Anda adalah angka positif"<<endl;
+    }
+
+
+  /*cout<<"Entry Data Lagi (Y/N) ?  ";
+    cin>>ulang;
+    cout<<endl;
+    if(ulang=='Y'||ulang=='y'){
+        goto awal;
+    }
+    if(ulang=='N'||ulang=='n'){
+        goto akhir;
+    }
+    akhir:
+
+
+    return 0;*/
+
+}
